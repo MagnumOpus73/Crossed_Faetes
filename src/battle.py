7e56932f0fe8df699df9dc0e163312a373c9ed03
MagnumOpus73 <47050@rws.uk.net>
2025-12-08 09:50:11 +0000

    Update battle.py

--- src/battle.py
+++ src/battle.py
@@ -1,3 +1,22 @@
+playerAction = 0
 def start_of_turn(turn_count):
   turn_count += 1
   
+  return turn_count
+
+
+
+
+while opponent.currentHealth != 0 and player.currentHealth != 0:
+  start_of_turn(turn_count)
+  while playerAction == 0:
+    playerAction = input("Would you like to attack (A), use an item(I), or flee(F)?").upper
+    if playerAction == "A":
+
+    elif playerAction == "I":
+
+    elif playerAction == "F":
+
+    else:
+      print("Invalid input: Try Again.")
+      playerAction = 0
