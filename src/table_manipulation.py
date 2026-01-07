1c2c0bc03a049ccf4bff493a3c7091b2c1684648
MagnumOpus73 <47050@rws.uk.net>
2026-01-07 11:47:44 +0000

    Update table_manipulation.py

--- src/table_manipulation.py
+++ src/table_manipulation.py
@@ -1,2 +1,12 @@
+#Importing Vital Libraries
 import json
 from pathlib import Path
+
+#Important functions for table manipulations
+def loadJSON(filepath):
+  with open(filepath, "r") as f:
+    return json.load(f)
+
+def saveJSON(filepath, data):
+  with open(filepath, "w") as f:
+    json.dump(data, f, indent = 4)
