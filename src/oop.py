70f13e352c67129f9606267494fd77ad407042ff
MagnumOpus73 <47050@rws.uk.net>
2026-01-29 13:05:24 +0000

    Update oop.py

--- src/oop.py
+++ src/oop.py
@@ -7,10 +7,10 @@ class Faerie:
     self.baseDefense = defense
     self.baseSpeed = speed
     self.canEvolve = canEvolve
-    self.HP = round((((hp * 2) + level)/160) + (level * 2) + 25)   #Turning base stats to actual stats via Mathematics.
-    self.Attack = round((((attack * 2) + level)/10) + level + 25)
-    self.Defense = round((((defense * 2) + level)/10) + level + 25)
-    self.Speed = round((((speed * 2) + level)/10) + level + 25)
+    self.HP = round((((hp * 2) + (level*20))/160) + (level * 20) + 25)   #Turning base stats to actual stats via Mathematics.
+    self.Attack = round((((attack * 2) + (level*20))/10) + (level*20) + 25)
+    self.Defense = round((((defense * 2) + (level*20))/10) + (level*20) + 25)
+    self.Speed = round((((speed * 2) + (level*20))/10) + (level*20) + 25)
 #Function for later and for testing.  
   def display(self):
     print(self.Name)
@@ -27,9 +27,10 @@ class Faerie:
     
 #Split between the class for database use and the ones being used by the user and enemies.    
 class InPlay_Faerie(Faerie):
-  def __init__(self, name, level, court, hp, attack, defense, speed, canEvolve, level):
+  def __init__(self, name, level, court, hp, attack, defense, speed, canEvolve, killCount):
     Faerie.__init__(self, name, level, court, hp, attack, defense, speed, canEvolve)
     self.Level = level
+    self.Kills = killCount
     self.currentHP = self.HP
     self.Fainted = False
     self.Poisoned = 0
@@ -44,12 +45,19 @@ class InPlay_Faerie(Faerie):
     elif self.currentHP > self.HP:
       self.currentHP = self.HP
 
-  def
+  def LevelUp(self):
+    if self.Level < 5 and self.Kills == (self.Level * 5):
+      self.Kills = 0
+      self.Level = self.Level + 1
+      print("Your", self.Name, "levelled up!")
+    else:
+      print("Your", self.Name, "got a kill! Nice!")
+
 
 #Actual testing. How statlines appear when going through the formulas.
-test1 = InPlay_Faerie("TEST", 1, "NULL", 80, 80, 80, 80, "False")
-test2 = InPlay_Faerie("TEST", 50, "NULL", 80, 80, 80, 80, "False")
-test3 = InPlay_Faerie("TEST", 100, "NULL", 80, 80, 80, 80, "False")
+test1 = InPlay_Faerie("TEST", 1, "NULL", 80, 80, 80, 80, "False", 0)
+test2 = InPlay_Faerie("TEST", 2, "NULL", 80, 80, 80, 80, "False", 0)
+test3 = InPlay_Faerie("TEST", 5, "NULL", 80, 80, 80, 80, "False", 0)
 InPlay_Faerie.display(test1)
 print()
 InPlay_Faerie.display(test2)
