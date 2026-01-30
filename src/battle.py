ce53fb4b07850827b73c463bc04822f898f936c5
MagnumOpus73 <47050@rws.uk.net>
2026-01-30 10:48:59 +0000

    Update battle.py
    Fixed Logic Error in Status Effects.

--- src/battle.py
+++ src/battle.py
@@ -1,20 +1,22 @@
+import src.oop
+
 playerAction = 0
 
 def damage(power, level, attack, enemy_defense):
-  return(((2*level + 2) * power * (attack/enemy_defense))
+  return(((2 * (level * 20) + 2) * power * (attack/enemy_defense))
 
 def start_of_turn(turn_count, self):
   turn_count += 0.5
-  if self.Burning <= 0:
+  if self.Burning > 0:
     self.currentHP = takeDamage((1/8 * self.currentHP), self)
     self.Burning -= 1
-  elif self.Poisoned <= 0:
+  elif self.Poisoned > 0:
     self.currentHP = takeDamage((1/16 * self.currentHP * (4 - self.Poisoned)), self)
     self.Poisoned -= 1
-  elif self.Regrowing <= 0:
+  elif self.Regrowing > 0:
     self.currentHP = takeDamage((-1 * (1/8 * self.currentHP), self))
     self.Regrowing -= 1
-  elif self.Withering <= 0:
+  elif self.Withering > 0:
     self.Withering -= 1
      if self.Withering > 0:
        print("You see your monster continue to wither away. ", self.Withering + "turns remaining.")
@@ -39,5 +41,15 @@ while opponent.currentHealth != 0 and player.currentHealth != 0:
     else:
       print("Invalid input: Try Again.")
       playerAction = 0
+if player.currentHealth == 0:
+  if player.ValidTeamNumber == 0:
+    #Player Loses
+  elif player.ValidTeamNumber > 0:
+    #Player sends out next member.
 
+if opponent.currentHealth == 0:
+  if opponent.ValidTeamNumber == 0:
+    #Player Wins.
+  elif opponent.ValidTeamNumber > 0:
+    #Opponent sends out next member.
 
