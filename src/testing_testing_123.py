efd84a1575a8da5b4d622ec97a462f7e6ca130dc
MagnumOpus73 <47050@rws.uk.net>
2026-03-13 10:49:23 +0000

    Contiued JSON Testing

--- src/testing_testing_123.py
+++ src/testing_testing_123.py
@@ -4,5 +4,6 @@ import table_manipulation as t
 import oop as o
 
 test = (t.loadJSON(filepath = "./src/creatures.json"))
-for i in test["faeries"]:
-    print(test["faeries"][i]["name"])
+for i in range(len(test["faeries"])):
+    if i % 2 != 0:
+        print(test["faeries"][i])
